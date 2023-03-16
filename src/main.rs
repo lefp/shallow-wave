8b6533359a2e9f260d6bcf6eaa96e8c9edce363c
Peter Lef <lefp@clarkson.edu>
2023-03-15 23:28:57 -0400

    Render using OpenCL

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,12 @@ use softbuffer::GraphicsContext;
 const WINDOW_WIDTH:  usize = 800;
 const WINDOW_HEIGHT: usize = 600;
 
+struct OclStuff {
+    pro_que: ocl::ProQue,
+    image_buffer: ocl::Buffer<u32>,
+    render_kernel: ocl::Kernel,
+}
+
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
@@ -24,9 +30,9 @@ fn main() {
                 .expect("Failed to build window.");
     // the graphics context is the abstraction through which images are written to the window, I think
     let mut graphics_context = unsafe { GraphicsContext::new(&window, &window) }.unwrap();
-
+    let ocl_stuff = set_up_opencl();
     // each u32 represents a color: 8 bits of nothing, then 8 bits each of R, G, B
-    let mut image_buffer = Box::new([0u32; WINDOW_WIDTH * WINDOW_HEIGHT]);
+    let mut image_buffer = vec![0u32; WINDOW_WIDTH * WINDOW_HEIGHT];
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll(); // Continuously runs the event loop, as opposed to `set_wait`
@@ -38,24 +44,46 @@ fn main() {
                 drawing code to the RedrawRequested arm of the match statement.
                 */
 
-                for linear_index in 0..image_buffer.len() {
-                    let x = linear_index % WINDOW_WIDTH;
-                    let y = linear_index / WINDOW_WIDTH;
-
-                    // `some_norm` is in [0,1]
-                    let some_norm = (x*x + y*y) as f32
-                                    / (WINDOW_WIDTH.pow(2) + WINDOW_HEIGHT.pow(2)) as f32;
-
-                    let red   = 0u32;
-                    let green = (255. * some_norm) as u32;
-                    let blue  = 255 - green;
-
-                    image_buffer[linear_index] = (red << 16) | (green << 8) | blue;
-                }
-
+                unsafe { ocl_stuff.render_kernel.enq().unwrap(); }
+                ocl_stuff.image_buffer.read(&mut image_buffer).enq().unwrap();
                 graphics_context.set_buffer(image_buffer.as_ref(), WINDOW_WIDTH as u16, WINDOW_HEIGHT as u16);
             },
             _ => {},
         }
     })
 }
+
+fn set_up_opencl() -> OclStuff {
+    let src = r#"
+        kernel void render(global uint* buffer, uint image_width, uint image_height) {
+            uint linear_index = get_global_id(0);
+            uint x = linear_index % image_width;
+            uint y = linear_index / image_width;
+
+            // `some_norm` is in [0,1]
+            float some_norm = (float)(x*x + y*y)
+                            / (float)(image_width*image_width + image_height*image_height);
+
+            uint red = 0;
+            uint green = (uint)(255.f * some_norm);
+            uint blue = 255 - green;
+            buffer[linear_index] = (red << 16) | (green << 8) | blue;
+        }
+    "#;
+
+    let pro_que = ocl::ProQue::builder()
+                  .src(src)
+                  .dims(WINDOW_WIDTH * WINDOW_HEIGHT)
+                  .build().unwrap();
+
+    let image_buffer = pro_que.create_buffer::<u32>().unwrap();
+
+    let render_kernel = pro_que.kernel_builder("render")
+                        .arg(&image_buffer)
+                        .arg(WINDOW_WIDTH  as u32)
+                        .arg(WINDOW_HEIGHT as u32)
+                        .build().unwrap();
+
+    OclStuff { pro_que, image_buffer, render_kernel }
+}
+
