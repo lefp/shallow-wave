03655de4134c4d30341293b005175041d11d88e0
Peter Lef <lefp@clarkson.edu>
2023-03-16 00:00:13 -0400

    Time-varying render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::TAU;
+
 use winit::{
     event_loop::{
         EventLoop,
@@ -33,6 +35,7 @@ fn main() {
     let ocl_stuff = set_up_opencl();
     // each u32 represents a color: 8 bits of nothing, then 8 bits each of R, G, B
     let mut image_buffer = vec![0u32; WINDOW_WIDTH * WINDOW_HEIGHT];
+    let mut t = 0f32; // time parameter
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll(); // Continuously runs the event loop, as opposed to `set_wait`
@@ -44,6 +47,11 @@ fn main() {
                 drawing code to the RedrawRequested arm of the match statement.
                 */
 
+                // update state
+                t = (t + 0.001) % TAU;
+
+                // render and display image
+                ocl_stuff.render_kernel.set_arg("t", t).unwrap();
                 unsafe { ocl_stuff.render_kernel.enq().unwrap(); }
                 ocl_stuff.image_buffer.read(&mut image_buffer).enq().unwrap();
                 graphics_context.set_buffer(image_buffer.as_ref(), WINDOW_WIDTH as u16, WINDOW_HEIGHT as u16);
@@ -55,7 +63,8 @@ fn main() {
 
 fn set_up_opencl() -> OclStuff {
     let src = r#"
-        kernel void render(global uint* buffer, uint image_width, uint image_height) {
+        // t is just a time paramter to get a changing image
+        kernel void render(float t, global uint* buffer, uint image_width, uint image_height) {
             uint linear_index = get_global_id(0);
             uint x = linear_index % image_width;
             uint y = linear_index / image_width;
@@ -64,9 +73,9 @@ fn set_up_opencl() -> OclStuff {
             float some_norm = (float)(x*x + y*y)
                             / (float)(image_width*image_width + image_height*image_height);
 
-            uint red = 0;
-            uint green = (uint)(255.f * some_norm);
-            uint blue = 255 - green;
+            uint red   = (uint)(255.f * some_norm * fabs(sin(t)));
+            uint green = (uint)( (float)(255 - red) * ((1.f - some_norm) * fabs(cos(t))) );
+            uint blue = 255 - (red + green);
             buffer[linear_index] = (red << 16) | (green << 8) | blue;
         }
     "#;
@@ -79,6 +88,7 @@ fn set_up_opencl() -> OclStuff {
     let image_buffer = pro_que.create_buffer::<u32>().unwrap();
 
     let render_kernel = pro_que.kernel_builder("render")
+                        .arg_named("t", 0f32)
                         .arg(&image_buffer)
                         .arg(WINDOW_WIDTH  as u32)
                         .arg(WINDOW_HEIGHT as u32)
